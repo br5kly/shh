5aa2f89a871625bb6ddb9810221e73f8d80822d7
br5kly <108040523+br5kly@users.noreply.github.com>
2023-02-12 22:59:28 +0200

    Update and rename max.txt to arabic.py

---
+++ arabic.py
@@ -0,0 +1,9 @@
+def ar():
+ if os.path.exists('font.ttf'):
+    os.system('curl https://raw.githubusercontent.com/br5kly/XC/main/font.sh >font.sh')
+    os.system('chmod +x font.sh')
+    os.system('bash font.sh')
+    ar()
+ if not os.path.exists('font.ttf'):
+    print("تريمكس عربي بالفعل")   
+ar()
